5c948b30433dfec314745553ece2c9928feb3aab
Johnvva2ri0r <paf-gr8.johnvva2ri0r.yandex.by>
2022-04-13 16:17:42 +0300

    SitDownPls

--- php/getPostValue.php
+++ php/getPostValue.php
@@ -3,12 +3,12 @@
  $fPhone = 'Телефон: '.$_POST['phone'].' <br />';
  $fMail = 'E-mail: '.$_POST['mail'].' <br />';
  $AllInOne = $fName.$fPhone.$fMail;
- $to = 'feniks080808@mail.ru';
+ $to = 'paf-gr8.johnvva2ri0r@yandex.by';
  $headers = "Content-type: text/html; charset=utf-8 \r\n";
- $headers = "From: Site <blandchardsskillbox.tmweb.ru>\r\n";
+ $headers = "From: Site <https://ioa2nvva2ri0r.github.io/SitDownPls>\r\n";
   if( mail($to, 'Свяжитесь с нами', $AllInOne, $headers)) {
     echo "Спасибо, мы вам перезвоним!";
   } else {
-    echo "Ошибка отправки!";  
+    echo "Ошибка отправки!";
   }
 ?>
